44b2ccbe8b1138bb6dad065ae79aea264bd9550e
hamlet <imhamlet.hams@gmail.com>
2021-01-19 23:30:07 +0545

    basic game screens

--- carlanegame/js/game.js
+++ carlanegame/js/game.js
@@ -4,6 +4,7 @@ function Player() {
     this.height = 100;
     this.index = 1;
     this.lanes = 3;
+    this.top = 0;
     this.y = 500;
     this.positions = [25, 125, 225];
     // this.isPlayer = false;
@@ -112,7 +113,8 @@ function Game() {
         this.field.style.width = this.width + 'px';
         this.field.style.height = this.height + 'px';
         this.field.style.backgroundColor = 'red';
-
+        this.field.style.backgroundImage = 'url("./images/lane.png")';
+        // this.field.style.backgroundImage ='url("./images/lane.png") 0 / '+ this.width+'px ' + this.height+ 'px';
         this.player = new Player();
         this.player.init();
         this.player.draw(this.field);
@@ -128,8 +130,9 @@ function Game() {
             this.enemies.push(enemy);
 
         }
-
-        this.update();
+        this.createScore();
+        this.createStartScreen();
+        // this.update();
         
         
     }
@@ -140,6 +143,17 @@ function Game() {
         enemy.init(this.getRandomValue(2,0), -55);
         enemy.draw(this.field);
         this.enemies.push(enemy);        
+    }
+
+    this.createScore = function() {
+        this.scoreText = document.createElement('p');
+        this.scoreBody = "Your Score: ";
+        this.scoreText.innerHTML = this.scoreBody;
+        this.scoreText.style.position = 'absolute';
+        this.scoreText.style.top = '5px';
+        this.scoreText.style.right = '5px';
+        this.scoreText.style.color = 'green';
+        this.field.appendChild(this.scoreText);
     }
 
     this.addListeners = function() {
@@ -163,6 +177,7 @@ function Game() {
     this.update = function() {
 
         this.gameLoop = setInterval(() => {
+           
             // dis.field.style.backgroundPosition = '0px + 2px';
             for(var i=0; i<this.enemies.length; i++) {
                 this.enemies[i].y += 5;
@@ -175,7 +190,7 @@ function Game() {
             //     this.createEnemy();
             // }, 5000);
             
-        }, 100);
+        }, 50);
         
     }
 
@@ -193,18 +208,80 @@ function Game() {
 
             console.log('collision');
             clearInterval(dis.gameLoop);
+            this.createGameOverScreen();
+            this.showGameOverScreen();
         }
     }
 
     this.detectBorderCollision = function(enemy) {
-        if(enemy.y >= this.height-enemy.height){
-            console.log('border');
-            this.enemies.shift();
-            enemy.destroy();
+        if(enemy.y == this.height-enemy.height){
             this.score++;
             console.log(this.score);
-            this.createEnemy();
+            this.scoreText.innerHTML = this.scoreBody + this.score;
+            
         }      
+        if(enemy.y >= this.height){
+            console.log('border');
+            
+            this.enemies.shift();
+            enemy.destroy();
+            this.createEnemy();
+        }
+    }
+
+    this.createStartScreen = function() {
+        this.startScreen = document.createElement('div');
+        this.startScreen.style.width = this.width+'px';
+        this.startScreen.style.height = this.height+'px';
+        // this.startScreen.style.display = 'none';
+        this.startScreen.style.textAlign = 'center';
+        this.startScreen.style.verticalAlign = 'center';
+        this.startScreen.style.backgroundColor = 'blue';
+        this.startScreen.style.position = 'absolute';
+        this.startScreen.style.lineHeight = this.height + 'px';
+        this.startScreen.style.zIndex = '1';
+        // this.startScreen.style.padding = '20px';
+        this.startScreen.innerHTML = 'Click Anywhere to Start';
+       
+        this.field.appendChild(this.startScreen);
+        this.startScreen.addEventListener('click', function(){
+            dis.startScreen.style.display = 'none';
+            dis.update();
+        });
+    }
+
+    this.createGameOverScreen = function() {
+        this.screen = document.createElement('div');
+        this.screen.style.width = this.width+'px';
+        this.screen.style.height = this.height+'px';
+        this.screen.style.display = 'none';
+        this.screen.style.textAlign = 'center';
+        this.screen.style.verticalAlign = 'center';
+        this.screen.style.backgroundColor = 'blue';
+        this.screen.style.position = 'absolute';
+        this.screen.style.lineHeight = this.height + 'px';
+        this.screen.style.zIndex = '1';
+        // this.screen.style.padding = '20px';
+        this.screen.innerHTML = 'game over';
+
+        var playAgainBtn = document.createElement('Button');
+        playAgainBtn.innerHTML = "Play Again?";
+        playAgainBtn.style.background ="white";
+        playAgainBtn.style.border = "none";
+        playAgainBtn.style.cursor="pointer";
+       
+        this.screen.appendChild(playAgainBtn);
+        playAgainBtn.addEventListener('click', function(){
+            dis.hideGameOverScreen();
+            // dis.init('field-1');
+        });
+        this.field.appendChild(this.screen);
+    }
+    this.showGameOverScreen = function() {
+        this.screen.style.display = 'block';
+    }
+    this.hideGameOverScreen = function() {
+        dis.screen.style.display = 'none';
     }
 
 
